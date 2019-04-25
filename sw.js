ca5c1ed25b5b987299389fefe2cc2ca79e127584
Genesise <genesis.fadel17@gmail.com>
2019-04-24 21:56:40 -0600

    Modified the db helper to images load safely

--- sw.js
+++ sw.js
@@ -86,6 +86,7 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
+      
       return response || fetch(event.request);
     })
   )
